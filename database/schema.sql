7e2597b5a3328ae2e6e835123fc682baa2f6f69e
Henry Pham <hqpham98@gmail.com>
2024-01-22 23:10:58 +0000

    initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,53 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userID" serial PRIMARY KEY,
+  "firstName" text,
+  "lastName" text,
+  "userName" text,
+  "hashedPassword" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "messages" (
+  "messageID" serial PRIMARY KEY,
+  "conversationID" text,
+  "userID" integer,
+  "messageContent" text,
+  "timestamp" timestamptz
+);
+
+CREATE TABLE "conversations" (
+  "conversationID" text,
+  "userID" integer,
+  PRIMARY KEY ("conversationID", "userID")
+);
+
+CREATE TABLE "friendships" (
+  "friendshipID" serial PRIMARY KEY,
+  "userID1" integer,
+  "userID2" integer
+);
+
+CREATE TABLE "requests" (
+  "requestID" serial PRIMARY KEY,
+  "senderID" integer,
+  "receiverID" integer,
+  "timestamp" timestamptz
+);
+
+ALTER TABLE "conversations" ADD FOREIGN KEY ("userID") REFERENCES "users" ("userID");
+
+ALTER TABLE "messages" ADD FOREIGN KEY ("userID") REFERENCES "users" ("userID")
+;
+ALTER TABLE "messages" ADD FOREIGN KEY ("conversationID", "userID") REFERENCES "conversations" ("conversationID", "userID");
+
+ALTER TABLE "requests" ADD FOREIGN KEY ("senderID") REFERENCES "users" ("userID");
+
+ALTER TABLE "requests" ADD FOREIGN KEY ("receiverID") REFERENCES "users" ("userID");
+
+ALTER TABLE "friendships" ADD FOREIGN KEY ("userID1") REFERENCES "users" ("userID");
+
+ALTER TABLE "friendships" ADD FOREIGN KEY ("userID2") REFERENCES "users" ("userID");
