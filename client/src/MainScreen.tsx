afd0badae2861c31d10bbe4fd21d2532679b961d
Henry Pham <hqpham98@gmail.com>
2024-01-29 22:33:58 +0000

    Implement grabbing conversations #4

--- client/src/MainScreen.tsx
+++ client/src/MainScreen.tsx
@@ -1,3 +1,74 @@
+import { io, Socket } from 'socket.io-client';
+import { useEffect, useState, useContext, useCallback } from 'react';
+import { AppContext } from '../src/components/AppContext';
+
 export function MainScreen() {
-  return <>hi</>;
+  const [socket, setSocket] = useState<Socket>();
+  const [message, setMessage] = useState('');
+  const [conversations, setConversations] = useState('');
+  const [chat, setChat] = useState('');
+  const [friends, setFriends] = useState('');
+  const [convLoaded, setConvLoaded] = useState(false);
+  const [chatLoaded, setChatLoaded] = useState(false);
+
+  useEffect(() => {
+    async function getConversations() {
+      const res = await fetch(
+        `/api/pigeon/conversations/${appContext.user?.userID}`
+      );
+      const convos = JSON.parse(await res.json());
+      const temp = convos.map((convo) => <li>conversationID: {convo}</li>);
+      setConversations(temp);
+      setConvLoaded(true);
+    }
+    // async function getChat() {
+    //   const res = await fetch(
+    //     `/api/pigeon/conversations/${appContext.user?.userID}`
+    //   );
+    // }
+
+    async function getFriends() {}
+
+    setSocket(io('http://localhost:8080'));
+    if (socket) {
+      socket.io.on('error', (error) => {
+        console.log(error);
+      });
+    }
+    return () => {
+      socket?.disconnect();
+    };
+  }, []);
+
+  const appContext = useContext(AppContext);
+
+  function handleClick() {
+    socket?.emit(
+      'message',
+      `message sent from ${appContext.user?.username}: ${message}`
+    );
+    setMessage('');
+  }
+
+  if (!(convLoaded && chatLoaded)) return null;
+
+  return (
+    <div>
+      <h1>Conversations</h1>
+      <ol>{conversations}</ol>
+      <h1>Friends</h1>
+      <ol>{friends}</ol>
+      <h1>Chat</h1>
+      <ol>{chat}</ol>
+      <h1>Messaging {}</h1>
+      <input
+        className="input input-bordered"
+        name="message"
+        value={message}
+        onChange={(e) => setMessage(e.currentTarget.value)}></input>
+      <button className="btn btn-primary" onClick={() => handleClick()}>
+        click me
+      </button>
+    </div>
+  );
 }
--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,9 @@ import express from 'express';
 import pg from 'pg';
 import argon2 from 'argon2';
 import jwt from 'jsonwebtoken';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
 import {
   ClientError,
   defaultMiddleware,
@@ -25,6 +28,12 @@ const hashKey = process.env.TOKEN_SECRET;
 if (!hashKey) throw new Error('TOKEN_SECRET not found in .env');
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+  },
+});
 
 // Create paths for static directories
 const reactStaticDir = new URL('../client/dist', import.meta.url).pathname;
@@ -34,6 +43,17 @@ app.use(express.static(reactStaticDir));
 // Static directory for file uploads server/public/
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
+
+//Get all conversations involving self
+app.get('/api/pigeon/conversations/:userID', async (req, res) => {
+  const { userID } = req.params;
+  const sql = `SELECT "conversationID"
+   FROM "conversations"
+   WHERE "userID" = $1`;
+  const params = [userID];
+  const result = await db.query(sql, params);
+  res.send(result.rows);
+});
 
 app.post('/api/pigeon/login', async (req, res, next) => {
   try {
@@ -85,6 +105,16 @@ app.use(defaultMiddleware(reactStaticDir));
 
 app.use(errorMiddleware);
 
-app.listen(process.env.PORT, () => {
-  process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('message', (msg) => {
+    console.log('message', msg);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  process.stdout.write(`\n\server listening on port ${process.env.PORT}\n\n`);
 });
