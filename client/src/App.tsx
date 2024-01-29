74ebb94462ee5f3cd4ff096fbf3fbd901757067a
Henry Pham <hqpham98@gmail.com>
2024-01-29 07:32:54 +0000

    Implement sign in into placeholder screen.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,55 @@
 // import { useEffect, useState } from 'react';
 import './App.css';
-import { LandingPage } from './LandingPage';
-import { SignupForm } from './SignupForm';
-import { SigninForm } from './SigninForm';
+import { LandingPage } from './pages/LandingPage';
+import { SignupForm } from './pages/SignupForm';
+import { SigninForm } from './pages/SigninForm';
 import { Route, Routes } from 'react-router-dom';
+import { User, Auth } from './lib/api';
+import { AppContext } from './components/AppContext';
+import { MainScreen } from './MainScreen';
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [user, setUser] = useState<User>();
+  const [token, setToken] = useState<string>();
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [isAuthorizing, setIsAuthorizing] = useState(true);
+
+  useEffect(() => {
+    const auth = localStorage.getItem('auth');
+    if (auth) {
+      const a = JSON.parse(auth);
+      setUser(a.user);
+      setToken(a.token);
+      setLoggedIn(true);
+    }
+    setIsAuthorizing(false);
+  }, []);
+
+  if (isAuthorizing) return null;
+
+  function handleSignIn(auth: Auth) {
+    localStorage.setItem('auth', JSON.stringify(auth));
+    setUser(auth.user);
+    setToken(auth.token);
+    setLoggedIn(true);
+  }
+
+  function handleSignOut() {
+    localStorage.removeItem('auth');
+    setUser(undefined);
+    setToken(undefined);
+    setLoggedIn(false);
+  }
+
+  const contextValue = { user, token, handleSignIn, handleSignOut };
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/register" element={<SignupForm />} />
-      <Route path="/login" element={<SigninForm />} />
-    </Routes>
+    <AppContext.Provider value={contextValue}>
+      <Routes>
+        <Route path="/" element={loggedIn ? <MainScreen /> : <LandingPage />} />
+        <Route path="/register" element={<SignupForm />} />
+        <Route path="/login" element={<SigninForm />} />
+      </Routes>
+    </AppContext.Provider>
   );
 }
