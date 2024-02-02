e641fb214ad44a29ba7f6a8f6c4208e8f742ea51
Henry Pham <hqpham98@gmail.com>
2024-02-02 09:35:28 +0000

    Changed environment variables

--- server/server.ts
+++ server/server.ts
@@ -33,7 +33,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:5173'
+        : process.env.REACT_APP_API_URL,
   },
 });
 
