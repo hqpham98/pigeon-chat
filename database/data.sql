35c2d6854ad2e6868b7d601398742866340bbf06
Henry Pham <hqpham98@gmail.com>
2024-04-19 08:24:17 +0000

    Update preset data

--- database/data.sql
+++ database/data.sql
@@ -11,7 +11,7 @@
 --      ('Get a job', false);
 
 INSERT INTO "users" ("firstName", "lastName", "email", "username", "hashedPassword")
-VALUES ('Bobby', 'Lee', 'taco@gmail.com', 'username', '$argon2i$v=19$m=4096,t=3,p=1$drFII7yZuHIyUFi8H+ei/A$0qzEImYgUE97RaUS4e+dh8mtxWibq3vmmU0FWABsnEE'), ('Henry', 'Pham', 'asdfadsf@gmail.com', 'henry', '$argon2i$v=19$m=4096,t=3,p=1$cSZGXBbtgw/czkO4+QvCqw$rF1gUTisBVdQJsFlJWNeQNRKIYqEi9X6Sj3pON7FudY');
+VALUES ('Bobby', 'Lee', 'taco@gmail.com', 'bobby', '$argon2i$v=19$m=4096,t=3,p=1$drFII7yZuHIyUFi8H+ei/A$0qzEImYgUE97RaUS4e+dh8mtxWibq3vmmU0FWABsnEE'), ('Henry', 'Pham', 'asdfadsf@gmail.com', 'henry', '$argon2i$v=19$m=4096,t=3,p=1$cSZGXBbtgw/czkO4+QvCqw$rF1gUTisBVdQJsFlJWNeQNRKIYqEi9X6Sj3pON7FudY');
 
 INSERT INTO "friendships" ("userID1", "userID2")
 VALUES (1, 2), (2, 1);
